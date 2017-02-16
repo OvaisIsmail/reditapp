0dff15e396fa18cc236d8680474416150e8d1d97
Ovais Ismail <Ovais.Ismail@markit.com>
2017-02-16 13:58:34 +0530

    initial comit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { ArticleComponent } from './article/article.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent // <-- added this
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
